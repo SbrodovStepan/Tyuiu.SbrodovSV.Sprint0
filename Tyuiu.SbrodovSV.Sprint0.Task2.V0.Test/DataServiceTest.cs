b97ee87d306f2c229fb5c7e3109dea07e949378a
Tyuiu.SbrodovSV@outlook.com <Tyuiu.SbrodovSV@outlook.com>
2023-09-06 17:01:12 +0500

    Task2

---
+++ Tyuiu.SbrodovSV.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SbrodovSV.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.SbrodovSV.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Степан";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Степан", res);
+        }
+    }
+}
